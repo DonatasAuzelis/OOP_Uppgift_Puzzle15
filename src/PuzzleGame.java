94c42d872be207b0e99af62c7f4d6882cc756d61
Donatas Auzelis <donatas963@gmail.com>
2020-10-29 11:32:26 +0100

    Added Main Class. Addes method for new game, and some small adjustments.

--- src/PuzzleGame.java
+++ src/PuzzleGame.java
@@ -34,6 +34,13 @@ public class PuzzleGame extends JFrame implements ActionListener {
         panel.add("West", optionsPanel);
         optionsPanel.setLayout(new GridLayout(2, 1));
 
+        newGameButton.addActionListener(this);
+        optionsPanel.add(newGameButton);
+
+        sortTilesButton.addActionListener(this);
+        optionsPanel.add(sortTilesButton);
+        sortTilesButton.setVisible(false);
+
         pack();
         setVisible(true);
         setLocationRelativeTo(null);
@@ -56,8 +63,27 @@ public class PuzzleGame extends JFrame implements ActionListener {
         }
     }
 
+    public void startNewGame() {
+        //Cleanup
+        gamePanel.removeAll();
+        gamePanel.revalidate();
+        gamePanel.repaint();
+
+        //Add methods
+        addAndShuffleTiles();
+        addButtonListeners();
+
+        //Frame settings
+        sortTilesButton.setVisible(true);
+        setSize(400, 350);
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == newGameButton)
+            startNewGame();
+        else if (e.getSource() == sortTilesButton){}
+        else {}
 
     }
 }
