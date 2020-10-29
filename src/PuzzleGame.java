e60619a0e456dc283a46bc588b8046091fe7f695
Donatas Auzelis <donatas963@gmail.com>
2020-10-29 11:16:54 +0100

    Added methods for game buttons and ActionListeners

--- src/PuzzleGame.java
+++ src/PuzzleGame.java
@@ -1,5 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Created by Donatas Auzelis
@@ -8,7 +12,7 @@ import java.awt.*;
  * Project: OOP_Sprint3_Inlämning
  * Copyright: MIT
  */
-public class PuzzleGame extends JFrame{
+public class PuzzleGame extends JFrame implements ActionListener {
 
     JPanel panel = new JPanel();
     JPanel gamePanel = new JPanel();
@@ -16,6 +20,8 @@ public class PuzzleGame extends JFrame{
 
     JButton newGameButton = new JButton("Nytt Spel");
     JButton sortTilesButton = new JButton("Sortera rätt");
+
+    ArrayList<JButton> gameTiles;
 
     public PuzzleGame() {
 
@@ -34,4 +40,24 @@ public class PuzzleGame extends JFrame{
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
+    public void addAndShuffleTiles() {
+        gameTiles = new ArrayList<>();
+        for (int i = 1; i < 16; i++) {
+            gameTiles.add(new JButton(i + ""));
+        }
+        gameTiles.add(new JButton());
+        Collections.shuffle(gameTiles);
+    }
+
+    public void addButtonListeners() {
+        for (JButton button: gameTiles) {
+            gamePanel.add(button);
+            button.addActionListener(this);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
